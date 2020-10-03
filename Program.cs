e126702dcc11fc6b9608ba91ea57051fe7864838
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-10-03 01:06:27 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entradaMilhas; 
+            double milhas, quilometros; 
+
+            Console.Write("entre com a medida (em milhas): "); 
+
+            entradaMilhas = Console.ReadLine();
+            milhas = Convert.ToDouble(entradaMilhas); 
+
+            quilometros = milhas * 1.609; 
+            Console.WriteLine($"o valor {milhas} milhaas correspondem a quilometros {quilometros}");
+            
+
+        }
+    }
+}
